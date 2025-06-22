cda67b8677818a6df98fad2a266b829cb5ef98a3
Lukasz Kurczab <lukasz.kurczab@gmail.com>
2025-06-22 17:30:26 +0200

    feat: Add login and registration pages with associated assets
    - Created login.html and register.html for user authentication.
    - Added login and register images (login-picture.png, register-picture.png).
    - Introduced 404.html for handling page not found errors.
    - Added index.html for Firebase Hosting setup.
    - Updated firebase.ts with new appId and measurementId.
    - Implemented storage.rules to restrict access to Firebase Storage.
    - Added login.txt and register.txt for tracking page metadata.

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,8 +8,8 @@ const firebaseConfig = {
   projectId: "friends-travel-3ea70",
   storageBucket: "friends-travel-3ea70.firebasestorage.app",
   messagingSenderId: "435646682540",
-  appId: "1:435646682540:web:34817268dfa41ebc9d1609",
-  measurementId: "G-DDTR3BRMC1",
+  appId: "1:435646682540:web:b98cee8f8a44915a9d1609",
+  measurementId: "G-564Y94G6RX",
 };
 
 const app = initializeApp(firebaseConfig);
